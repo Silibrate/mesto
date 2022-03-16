f00071ff52a8d809f22eb55a9e2aa7b7b246c752
Silibrate <asasins.90@mail.ru>
2022-03-17 02:09:26 +0300

    make all

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const popupEdit = document.querySelector('.popup-edit');
 const profileButtonEdit = document.querySelector('.profile__button-edit');
 const popupClos = popupEdit.querySelector('.popup__close');
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input-name");
+let jobInput = document.querySelector(".popup__input_user_Job");
+let card = document.querySelector('.card');
+let popupButton = document.querySelector('.popup__button');
 
 function popupOpen() {
   popupEdit.style.display = 'block';
 }
 
 function buttonClose() {
-  popupEdit.style.display = 'none'
+  popupEdit.style.display = 'none';
 }
 
-profileButtonEdit.addEventListener('click', popupOpen);
+profileButtonEdit.addEventListener('click', () =>{
+  popupOpen();
+});
+nameInput.value = document.querySelector(".profile__title").textContent;
+jobInput.value = document.querySelector(".profile__subtitle").textContent;
+
 popupClos.addEventListener('click', buttonClose);
 
 const ESC_KEY = "Escape";
@@ -21,23 +31,16 @@ document.addEventListener('keyup', function (event) {
   }
 })
 
-
-let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__input-name");
-let jobInput = document.querySelector(".popup__input_user_Job");
-
-nameInput.value = document.querySelector(".popup__input-name").textContent;
-jobInput.value = document.querySelector(".popup__input_user_Job").textContent;
-
 function formSubmitHandler(evt) {
-    evt.preventDefault();
-    document.querySelector(".profile__title").textContent = nameInput.value;
-    document.querySelector(".profile__subtitle").textContent = jobInput.value;
+  evt.preventDefault();
+  document.querySelector(".profile__title").textContent = nameInput.value;
+  document.querySelector(".profile__subtitle").textContent = jobInput.value;
 }
+
 formElement.addEventListener('submit', formSubmitHandler);
+popupButton.addEventListener('click', formSubmitHandler);
 
 
-
-
-
-
+card.querySelector('.card__button').addEventListener('click', function(evt){
+  evt.target.classList.toggle('card__button_on');
+});/* я не понимаю по чему лайкается только первая карточка*/
