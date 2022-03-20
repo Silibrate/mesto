de66c52973c3c1ff07bebb1916e1fbde31baf5b8
Silibrate <asasins.90@mail.ru>
2022-03-20 04:28:12 +0300

    fix bugs

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ let popupButton = document.querySelector('.popup__button');
 
 function popupOpen() {
   popupEdit.style.display = 'block';
+  nameInput.value = document.querySelector(".profile__title").textContent;
+  jobInput.value = document.querySelector(".profile__subtitle").textContent;
 }
 
 function buttonClose() {
@@ -18,8 +20,7 @@ function buttonClose() {
 profileButtonEdit.addEventListener('click', () =>{
   popupOpen();
 });
-nameInput.value = document.querySelector(".profile__title").textContent;
-jobInput.value = document.querySelector(".profile__subtitle").textContent;
+
 
 popupClos.addEventListener('click', buttonClose);
 
@@ -35,12 +36,13 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   document.querySelector(".profile__title").textContent = nameInput.value;
   document.querySelector(".profile__subtitle").textContent = jobInput.value;
+  buttonClose();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 popupButton.addEventListener('click', formSubmitHandler);
 
-
+/*
 card.querySelector('.card__button').addEventListener('click', function(evt){
   evt.target.classList.toggle('card__button_on');
-});/* я не понимаю по чему лайкается только первая карточка*/
+}); :( */
